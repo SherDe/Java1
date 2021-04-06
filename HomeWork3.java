272a70725664f68b2d44243c7cc4b6a8a00e8dd2
SherDe <81530199+SherDe@users.noreply.github.com>
2021-04-06 20:31:50 +0300

    Add files via upload

---
+++ HomeWork3.java
@@ -0,0 +1,33 @@
+public class HomeWork3 {
+    public static void main(String[] args) {
+
+        System.out.println (HomeWork1( 5,10));
+        HomeWork2(a: -13);
+        System.out.println(HomeWork3(a: 10203))
+
+    }}
+
+    static boolean HomeWork1 (int a, int b) {
+        System.out.println ();
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+
+    }
+}
+    static void HomeWork2(int a) {
+        String word = "Положительное";
+        if (a < 0) word = "Отрицательное";
+        System.out.println(word);
+    }
+}
+    static boolean HomeWork3(int a) {
+        return a < 0;
+        }
+        }
+
+      static void HomeWork4(int a) {
+        String s = ("Домашнее задание");
+        for (int i = 0; i < 3; i++) {
+        System.out.println(s);
+
+        }}}
